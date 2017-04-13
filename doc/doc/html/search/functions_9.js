8c9a56b30f4fdbb9acb98e9ed33e9e37db61acab
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-04-13 18:59:33 -0400

    doc

---
+++ doc/doc/html/search/functions_9.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['label',['Label',['../classsfg_1_1Label.html#a63d9c40e3e266c2a6d74f0628b165d59',1,'sfg::Label']]],
+  ['loadfont',['LoadFont',['../classsfg_1_1FileResourceLoader.html#a8e8fbd0a3dd3f1430a007db5a5a3aa7b',1,'sfg::FileResourceLoader::LoadFont()'],['../classsfg_1_1ResourceLoader.html#a8d808e91481a71f8aefec9ab08df6db7',1,'sfg::ResourceLoader::LoadFont()']]],
+  ['loadimage',['LoadImage',['../classsfg_1_1FileResourceLoader.html#a8b39f6348fcc45695475ac04572cca10',1,'sfg::FileResourceLoader::LoadImage()'],['../classsfg_1_1ResourceLoader.html#a9a4e0b7f0b2a1b8c7eb013288a9bdd85',1,'sfg::ResourceLoader::LoadImage()']]],
+  ['loadthemefromfile',['LoadThemeFromFile',['../classsfg_1_1Desktop.html#af553370eb6c2e511cbbcbb212de31a00',1,'sfg::Desktop::LoadThemeFromFile()'],['../classsfg_1_1Engine.html#a9f5045345c7fe4868f38764f843835bb',1,'sfg::Engine::LoadThemeFromFile()']]],
+  ['loadthemefromstring',['LoadThemeFromString',['../classsfg_1_1Engine.html#a5a06fa8c872f265af1c99852ae40f8a4',1,'sfg::Engine']]]
+];
